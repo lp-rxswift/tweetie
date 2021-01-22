500e10463c45a90c52e06188623e49781854365b
Lucas <lucaspedrazoli@gmail.com>
2021-01-22 14:52:32 -0300

    add view model test

--- TweetieTests/ListTimelineViewModelTests.swift
+++ TweetieTests/ListTimelineViewModelTests.swift
@@ -75,4 +75,20 @@ class ListTimelineViewModelTests: XCTestCase {
     XCTAssertTrue(emitted.0.count == 3)
   }
 
+  func test_whenAccountAvailable_updatesAccountStatus() {
+    let accountSubject = PublishSubject<TwitterAccount.AccountStatus>()
+    let viewModel = createViewModel(accountSubject.asDriver(onErrorJustReturn: .unavailable))
+    let loggedIn = viewModel.loggedIn.asObservable().materialize()
+    DispatchQueue.main.async {
+      accountSubject.onNext(.authorized(AccessToken()))
+      accountSubject.onNext(.unavailable)
+      accountSubject.onCompleted()
+    }
+    let emitted = try! loggedIn.take(3).toBlocking(timeout:
+    1).toArray()
+    XCTAssertEqual(emitted[0].element, true)
+    XCTAssertEqual(emitted[1].element, false)
+    XCTAssertTrue(emitted[2].isCompleted)
+  }
+
 }
