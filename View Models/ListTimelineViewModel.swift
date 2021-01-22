184f815609a5d001630d8246e6ea352680f15d3f
Lucas <lucaspedrazoli@gmail.com>
2021-01-22 14:43:27 -0300

    view model implementation

--- View Models/ListTimelineViewModel.swift
+++ View Models/ListTimelineViewModel.swift
@@ -40,26 +40,51 @@ import RxCocoa
 class ListTimelineViewModel {
   private let bag = DisposeBag()
   private let fetcher: TimelineFetcher
+  let list: ListIdentifier
+  let account: Driver<TwitterAccount.AccountStatus>
 
   // MARK: - Input
-
+  var paused: Bool = false {
+    didSet {
+      fetcher.paused.accept(paused)
+    }
+  }
   // MARK: - Output
+  private(set) var tweets: Observable<(AnyRealmCollection<Tweet>, RealmChangeset?)>!
+  private(set) var loggedIn: Driver<Bool>!
 
   // MARK: - Init
   init(account: Driver<TwitterAccount.AccountStatus>,
        list: ListIdentifier,
        apiType: TwitterAPIProtocol.Type = TwitterAPI.self) {
-
+    self.list = list
+    self.account = account
     // fetch and store tweets
     fetcher = TimelineFetcher(account: account, list: list, apiType: apiType)
     bindOutput()
+
+    fetcher.timeline
+      .subscribe(Realm.rx.add(update: .all))
+      .disposed(by: bag)
 
   }
 
   // MARK: - Methods
   private func bindOutput() {
     // Bind tweets
+    guard  let realm = try? Realm() else {
+      return
+    }
+    tweets = Observable.changeset(from: realm.objects(Tweet.self))
 
     // Bind if an account is available
+    loggedIn = account
+      .map { status in
+        switch status {
+        case .unavailable: return false
+        case .authorized: return true
+        }
+      }
+      .asDriver(onErrorJustReturn: false)
   }
 }
